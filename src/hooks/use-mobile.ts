4d39e5a209667db11103910890704fce7c1285cc
anhtudo97 <anhtudo97@gmail.com>
2025-12-16 14:43:24 +0700

    chore: update style prettier

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,25 +1,28 @@
-import * as React from "react";
+import * as React from "react"
 
-const MOBILE_BREAKPOINT = 768;
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const mediaQuery = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`;
+  const mediaQuery = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
-  const subscribe = React.useCallback((callback: () => void) => {
-    if (typeof window !== 'undefined') {
-      const mql = window.matchMedia(mediaQuery);
-      mql.addEventListener('change', callback);
-      return () => mql.removeEventListener('change', callback);
-    }
-    return () => { };
-  }, [mediaQuery]);
+  const subscribe = React.useCallback(
+    (callback: () => void) => {
+      if (typeof window !== "undefined") {
+        const mql = window.matchMedia(mediaQuery)
+        mql.addEventListener("change", callback)
+        return () => mql.removeEventListener("change", callback)
+      }
+      return () => {}
+    },
+    [mediaQuery]
+  )
 
   const getSnapshot = React.useCallback(() => {
-    if (typeof window !== 'undefined') {
-      return window.matchMedia(mediaQuery).matches;
+    if (typeof window !== "undefined") {
+      return window.matchMedia(mediaQuery).matches
     }
-    return false; // SSR fallback
-  }, [mediaQuery]);
+    return false // SSR fallback
+  }, [mediaQuery])
 
-  return React.useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  return React.useSyncExternalStore(subscribe, getSnapshot, getSnapshot)
 }
