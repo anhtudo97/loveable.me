18a1924f5e718c6e7420bb1552408488f9012a9e
anhtudo97 <anhtudo97@gmail.com>
2025-12-29 16:28:13 +0700

    chore: update style

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,6 @@
-import { Sandbox } from "@e2b/code-interpreter";
-import { createAgent, openai } from "@inngest/agent-kit";
-import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter"
+import { createAgent, openai } from "@inngest/agent-kit"
+import { inngest } from "./client"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -8,26 +8,25 @@ export const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       // Imagine this is a sandbox creation step
-      const sandbox = await Sandbox.create("cwa-loveable-test-1");
-      return sandbox.sandboxId;
-    });
+      const sandbox = await Sandbox.create("cwa-loveable-test-1")
+      return sandbox.sandboxId
+    })
 
     const codeAgent = createAgent({
-      name: 'Database summarizer',
-      description: 'Provides expert support for managing PostgreSQL databases',
+      name: "Database summarizer",
+      description: "Provides expert support for managing PostgreSQL databases",
       system:
-        'You are a PostgreSQL expert database administrator. ' +
-        'You only provide answers to questions related to PostgreSQL database schema, indexes, and extensions.',
+        "You are a PostgreSQL expert database administrator. " +
+        "You only provide answers to questions related to PostgreSQL database schema, indexes, and extensions.",
       model: openai({ model: "gpt-4o" })
-    });
-
+    })
 
     const { output } = await codeAgent.run(
       `Provide a brief summary of the database schema for sandbox with ID: ${sandboxId}`
-    );
+    )
 
-    console.log("Summary:", output);
+    console.log("Summary:", output)
 
-    return { message: `Hello ${event.data.email}!` };
+    return { message: `Hello ${event.data.email}!` }
   }
-);
+)
