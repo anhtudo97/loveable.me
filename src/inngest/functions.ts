535480a82df8d35a1e9dfae3cc55ec55f8c61a31
anhtudo97 <anhtudo97@gmail.com>
2025-12-29 15:08:35 +0700

    feat: setup agent

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,33 @@
-import { inngest } from "./client"
+import { Sandbox } from "@e2b/code-interpreter";
+import { createAgent, openai } from "@inngest/agent-kit";
+import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s")
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      // Imagine this is a sandbox creation step
+      const sandbox = await Sandbox.create("cwa-loveable-test-1");
+      return sandbox.sandboxId;
+    });
 
-    // Imagine this is a transcription step
-    await step.sleep("wait-a-moment", "10s")
+    const codeAgent = createAgent({
+      name: 'Database summarizer',
+      description: 'Provides expert support for managing PostgreSQL databases',
+      system:
+        'You are a PostgreSQL expert database administrator. ' +
+        'You only provide answers to questions related to PostgreSQL database schema, indexes, and extensions.',
+      model: openai({ model: "gpt-4o" })
+    });
 
-    // Imagine this is a summary step
-    await step.sleep("wait-a-moment", "10s")
 
-    return { message: `Hello ${event.data.email}!` }
+    const { output } = await codeAgent.run(
+      `Provide a brief summary of the database schema for sandbox with ID: ${sandboxId}`
+    );
+
+    console.log("Summary:", output);
+
+    return { message: `Hello ${event.data.email}!` };
   }
-)
+);
